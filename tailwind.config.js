0c7d34616f0ed6046c9749f6354b97a04ee17f14
Aleksandr2905 <natalukha@ukr.net>
2024-09-04 23:14:50 +0300

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        brotesk: "Brotesk, sans-serif",
+        darkerBold: "Darker-Bold, sans-serif",
+        darkerMedium: "Darker-Medium, sans-serif",
+        dmSans: "DMSans, sans-serif",
+        mustica: "Mustica, sans-serif",
+        syne: "Syne, sans-serif",
+        transforma: "Transforma, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
